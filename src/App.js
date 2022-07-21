8019ed3905f3cadf1750d5199debc8b3a6b2320f
Junyu Xu <junyuxu2006@gmail.com>
2022-07-21 18:19:07 -0500

    hi

---
+++ src/App.js
@@ -0,0 +1,259 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="AppHeaderBar">
+        
+      </div>
+      <div className="PointIcon">
+          
+        </div>
+        <div className="Vector">
+          
+          </div>
+        <div className="Rectangle468">
+          <img src={require("./Rectangle468.png")} />
+        </div>
+        <div className="TextSearch">
+        <img src={require("./SearchAnything.png")} />
+        </div>
+        <div className="SearchVector">
+        <img src={require("./SearchVector.png")} />
+        </div>
+        <div className="Line">
+        <img src={require("./Line.png")} />
+        </div>
+        <div className="NewLogo">
+        <img src={require("./NewLogo.png")} />
+        </div>
+        <div className="New">
+        <img src={require("./NEW.png")} />
+        </div>
+        <div className="HotLogo">
+        <img src={require("./HotLogo.png")} />
+        </div>
+        <div className="Hot">
+        <img src={require("./HOT.png")} />
+        </div>
+        <div className="JoinedLogo">
+        <img src={require("./JoinedLogo.png")} />
+        </div>
+        <div className="Joined">
+        <img src={require("./Joined.png")} />
+        </div>
+        <div className="SavedLogo">
+        <img src={require("./SavedLogo.png")} />
+        </div>
+        <div className="Saved">
+        <img src={require("./Saved.png")} />
+        </div>
+        <div className="NotificationLogo">
+        <img src={require("./NotificationLogo.png")} />
+        </div>
+        <div className="Notification">
+        <img src={require("./Notification.png")} />
+        </div>
+        <div className="CreateRound">
+        <img src={require("./CreateRound.png")} />
+        </div>
+        <div className="CRPostLogo">
+        <img src={require("./CRPostLogo.png")} />
+        </div>
+        <div className="CreatePost">
+        <img src={require("./CreatePost.png")} />
+        </div>
+        <div className="CRPostPic">
+        <img src={require("./CRPostPic.png")} />
+        </div>
+        <div className="RectPopCom">
+        <img src={require("./RectPopCom.png")} />
+        </div>
+        <div className="PopCom">
+        <img src={require("./PopCom.png")} />
+        </div>
+        <div className="LinePopCom">
+        <img src={require("./LineShort.png")} />
+        </div>
+        <div className="ComList">
+        <img src={require("./ComList.png")} />
+        </div>
+        <div className="LBoardRect">
+        <img src={require("./LBoardRect.png")} />
+        </div>
+        <div className="Leaderboard">
+        <img src={require("./Leaderboard.png")} />
+        </div>
+        <div className="LBoardTrophy">
+        <img src={require("./LBoardTrophy.png")} />
+        </div>
+        <div className="LBoardLine1">
+        <img src={require("./LineShort.png")} />
+        </div>
+        <div className="JohnDoe1">
+        <img src={require("./JohnDoe.png")} />
+        </div>
+        <div className="MedalGold">
+        <img src={require("./MedalGold.png")} />
+        </div>
+        <div className="TimeLong">
+        <img src={require("./TimeLong.png")} />
+        </div>
+        <div className="TimeMedium">
+        <img src={require("./TimeMedium.png")} />
+        </div>
+        <div className="TimeShort">
+        <img src={require("./TimeShort.png")} />
+        </div>
+        <div className="JohnDoe2">
+        <img src={require("./JohnDoe.png")} />
+        </div>
+        <div className="JohnDoe3">
+        <img src={require("./JohnDoe.png")} />
+        </div>
+        <div className="LBoardLine2">
+        <img src={require("./LineShort.png")} />
+        </div>
+        <div className="LBoardLine3">
+        <img src={require("./LineShort.png")} />
+        </div>
+        <div className="LBoardLine4">
+        <img src={require("./LineShort.png")} />
+        </div>
+        <div className="MedalSilver">
+        <img src={require("./MedalSilver.png")} />
+        </div>
+        <div className="MedalBronze">
+        <img src={require("./MedalBronze.png")} />
+        </div>
+        <div className="RectDM">
+        <img src={require("./RectDM.png")} />
+        </div>
+        <div className="DM">
+        <img src={require("./DM.png")} />
+        </div>
+        <div className="DMArrow">
+        <img src={require("./DMArrow.png")} />
+        </div>
+        <div className="PostRect1">
+        <img src={require("./PostRect.png")} />
+        </div>
+        <div className="PostRect2">
+        <img src={require("./PostRect.png")} />
+        </div>
+        <div className="PostRect3">
+        <img src={require("./PostRect.png")} />
+        </div>
+        <div className="PostUpvote1">
+        <img src={require("./PostUpvote.png")} />
+        </div>
+        <div className="PostDownvote1">
+        <img src={require("./PostDownvote.png")} />
+        </div>
+        <div className="PostUpvote2">
+        <img src={require("./PostUpvote.png")} />
+        </div>
+        <div className="PostUpvote3">
+        <img src={require("./PostUpvote.png")} />
+        </div>
+        <div className="PostDownvote2">
+        <img src={require("./PostDownvote.png")} />
+        </div>
+        <div className="PostDownvote3">
+        <img src={require("./PostDownvote.png")} />
+        </div>
+        <div className="Extracurriculars">
+        Extracurriculars
+        </div>
+        <div className="PostedBy">
+        Posted by Sharon Mitchell
+        </div>
+        <div className="StudentGreen">
+        <img src={require("./StudentGreen.png")} />
+        </div>
+        <div className="StudentText">
+        Student
+        </div>
+        <div className="TimeAgo">
+          5 minutes ago
+        </div>
+        <div className="PostContent">
+        Can anyone give me any tips on how to be a better softball player?
+        </div>
+        <div className="ReplyLogo">
+        <img src={require("./ReplyLogo.png")} />
+        </div>
+        <div className="Reply">
+          Reply
+        </div>
+        <div className="RepostLogo">
+        <img src={require("./RepostLogo.png")} />
+        </div>
+        <div className="Repost">
+          Repost
+        </div>
+        <div className="SaveLogo">
+        <img src={require("./SaveLogo.png")} />
+        </div>
+        <div className="Save">
+          Save
+        </div>
+        <div className="SendLogo">
+        <img src={require("./SendLogo.png")} />
+        </div>
+        <div className="Send">
+          Send
+        </div>
+        <div className="ReplyLogo2">
+        <img src={require("./ReplyLogo.png")} />
+        </div>
+        <div className="Reply2">
+          Reply
+        </div>
+        <div className="RepostLogo2">
+        <img src={require("./RepostLogo.png")} />
+        </div>
+        <div className="Repost2">
+          Repost
+        </div>
+        <div className="SaveLogo2">
+        <img src={require("./SaveLogo.png")} />
+        </div>
+        <div className="Save2">
+          Save
+        </div>
+        <div className="SendLogo2">
+        <img src={require("./SendLogo.png")} />
+        </div>
+        <div className="Send2">
+          Send
+        </div>
+        <div className="ReplyLogo3">
+        <img src={require("./ReplyLogo.png")} />
+        </div>
+        <div className="Reply3">
+          Reply
+        </div>
+        <div className="RepostLogo3">
+        <img src={require("./RepostLogo.png")} />
+        </div>
+        <div className="Repost3">
+          Repost
+        </div>
+        <div className="SaveLogo3">
+        <img src={require("./SaveLogo.png")} />
+        </div>
+        <div className="Save3">
+          Save
+        </div>
+        <div className="SendLogo3">
+        <img src={require("./SendLogo.png")} />
+        </div>
+        <div className="Send3">
+          Send
+        </div>
+    </div>
+  );
+}
+
+export default App;
