25b58a4694993f634536d5f3ae5bdbe45b524596
aaronthechen <aaronchen31415@gmail.com>
2022-07-20 23:44:32 -0500

    added ruby to top navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import '../App.css';
+import Ruby from '../Assets/Ruby.png'
+import Number from '../Assets/Number.png'
 
 export default function Navbar() {
   return (
-    <div className='navbar'></div>
+    <div className='navbar'>
+      <img className='ruby' src={Ruby} alt="ruby" />
+      <img className='ruby-number' src={Number} alt="ruby-number" />
+    </div>
+    
   )
 }
