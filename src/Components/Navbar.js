924de68708ec2ad212a0d05fb3211dd4e174e50c
andy <andrew.t.wang14@gmail.com>
2022-07-21 18:39:38 -0400

    commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,10 +4,12 @@ import Ruby from '../Assets/Ruby.png'
 import Number from '../Assets/Number.png'
 
 export default function Navbar() {
+  const rubyNumber = 1000;
+  
   return (
     <div className='navbar'>
-      //<img className='ruby' src={Ruby} alt="ruby" />
-      <img className='ruby-number' src={Number} alt="ruby-number" />
+      <img className='ruby' src={Ruby} alt="ruby" />
+      <h1 className='ruby-number'>{rubyNumber}</h1>
     </div>
     
   )
