2153e6ad37281c5161460fa7dba372787afdb3a8
stanleymw <stanleymaverwang@gmail.com>
2022-08-03 17:51:14 -0700

    prevent purchasing of the same item mulitiple times

--- src/Components/ShopItem.js
+++ src/Components/ShopItem.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import Ruby from '../Assets/Ruby.png'
 import Background from '../Assets/Background.png'
+import { useState } from 'react'
 
 const ShopItem = (props) => {
+  // props.count = price of item
+  let [purchased, setPurchased] = useState(false)
+
+  const purchaseItem = () => {
+    if (purchased) {
+      return
+    }
+    props.setRubyCount(props.count + props.rubyCount)
+    setPurchased(true)
+  }
+
   return (
     <div className="item-frame">
       <img src={Background} alt="Image" className='item-image'/>
@@ -10,9 +22,12 @@ const ShopItem = (props) => {
         <h2>{props.description}</h2>
         <a className='ruby-title-about' href="">View on full screen</a>
       </div>
-      <button className='ruby-button' onClick={() => props.setRubyCount(props.count + props.rubyCount)}>
-        <h2>{props.count}</h2>
-        <img src={Ruby} alt="ruby"></img>
+      <button className='ruby-button' onClick={() => purchaseItem()} style={{"backgroundColor": purchased ? "grey" : ""}}>
+        <h2>{!purchased ? props.count : "Purchased"}</h2>
+        {
+          !purchased ?
+          (<img src={Ruby} alt="ruby"></img>) : ""
+        }
       </button>
     </div>
   )
